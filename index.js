0bea71a0e2d44f063f041d9ebd290e244fe31594
[phanu] <[phanuelotiato21@gmail.com]>
2019-06-04 12:49:17 -0700

    add express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
  
 app.get("/", function (req, res){
     //logic
-    res.("Hello world");
+    res.send("Hello world");
 });
 app.listen(900, function () {
     console.log("App listening on port 9000");
