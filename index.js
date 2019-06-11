59f79cfdbb233b20eb404c4272e495259c5b4c7a
phanuelbonke <phanuelotiato21@gmail.com>
2019-06-11 19:42:03 +0300

    query for library-server

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const pool = mysql.createPool({
-         host: "localhost",
-         port:"9000",
-         user: "root",
-         password: "password",
-        database: "library"
+        host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
      });
 app.get("/api/library", (req, res)=> {
     //fetch publishers from database
-    pull.query("SELECT * FROM publisher", (error, rows)=> {
+    pool.query("SELECT * FROM publisher", (error, rows)=> {
+        console.log(error, rows)
         if (error){
             return res.status(500).json({ error });
         }
-        res.json(rows);
+        res.json(rows)
     });
 });
 
++ app.get("/api/publisher/:id", (req, res) => {
+         pool.query(
+             "SELECT id, name FROM publisher WHERE id = ?",
+             [req.params.id],
+             (error, rows) => {
+                 if (error) {
+                     return res.status(500).json({ error });
+                 }
+    
+                 res.json(rows);
+             }
+         );
+     });
 app.listen(9000, function () {
     console.log("App listening on port 9000");
 });
