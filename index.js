4aedbe4592066a823ff26491a5bc65cf7df6534e
phanuelbonke <phanuelotiato21@gmail.com>
2019-06-11 13:00:09 +0300

    mysql

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
- 
-app.get("/", function (req, res){
-    //logic
-    res.send("Hello world");
+const pool = mysql.createPool({
+         host: "localhost",
+         port:"9000",
+         user: "root",
+         password: "password",
+        database: "library"
+     });
+app.get("/api/library", (req, res)=> {
+    //fetch publishers from database
+    pull.query("SELECT * FROM publisher", (error, rows)=> {
+        if (error){
+            return res.status(500).json({ error });
+        }
+        res.json(rows);
+    });
 });
-app.listen(900, function () {
+
+app.listen(9000, function () {
     console.log("App listening on port 9000");
 });
