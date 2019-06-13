f0286e4bbecdfec888f58c41db9eae5810d128a9
phanuelbonke <phanuelotiato21@gmail.com>
2019-06-13 23:25:53 +0300

    updates

--- index.js
+++ index.js
@@ -31,7 +31,52 @@ app.get("/api/library", (req, res)=> {
                  res.json(rows);
              }
          );
-     });
+     }); app.get("/api/book/:id", (req, res) => {
+        pool.query(
+            "SELECT book_id, name FROM book WHERE id = ?",
+            [req.params.id],
+            (error, rows) => {
+                if (error) {
+                    return res.status(500).json({ error });
+                }
+   
+                res.json(rows);
+            }
+        );
+    });
+    app.get("/api/library/:id/publisher", (req, res) => {
+             pool.query(
+                 `SELECT pub_id, pub_name 
+                 FROM publisher`,
+                 [req.params.id],
+                 (error, rows) => {
+                     if (error) {
+                         return res.status(500).json({ error });
+                     }
+        
+                     res.json(rows);
+                 }
+             );
+         });
+         app.get("/api/library/:id/publisher", (req, res) => {
+                pool.query(
+                     `SELECT m.pub_id, m.pub_name, m.pub_info, GROUP_CONCAT(g.book_id) book_auther
+                     FROM publisher s
+                     JOIN book_auther g ON g.book_auther_id = m.id
+                     WHERE s.book_id = ?
+                     GROUP BY m.id,
+                     ORDER BY m.pub_info,`,
+                     [req.params.id],
+                     (error, rows) => {
+                         if (error) {
+                             return res.status(500).json({ error });
+                         }
+            
+                         res.json(rows);
+                     }
+                 );
+             });
+    
         
 app.listen(9000, function () {
     console.log("App listening on port 9000");
