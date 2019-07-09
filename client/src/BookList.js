25cadbd296bcd15520f5c547ab4f76e02fef0fa6
phanuelbonke <phanuelotiato21@gmail.com>
2019-07-09 12:14:34 +0300

    added static

--- client/src/BookList.js
+++ client/src/BookList.js
@@ -1,16 +1,62 @@
 import React from "react";
+import axios from "axios";
 import Books from "./Books";
+import Loading from "./loading";
+import Error from "./Error";
 
-function BookList({ booksInfo }) {
+class BookList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+booksInfo: [],
+loading: false,
+error: false
+        };
+    }
+
+componetDidmount() {
+    this.fetchBooksInfo();
+    }
+fetchBooksInfo() {
+    this.setState({ loading: true, error: false});
+    axios
+    .get("http://localhost;9000/api/booksinfo")
+    .get("/api/booksinfo")
+    .then(response=>{
+        this.setState({
+        booksInfo: response.data,
+        loading: false,
+        error: false
+    });
+})
+.catch(error => {
+    this.setState({
+        booksInfo:[],
+        loading: false,
+        error: true
+    });
+});
+}
+render() {
+    const {booksInfo, loading, error } = this.state;
+    if (loading){
+        return <Loading />;
+    }
+    if (error){
+        return <Error />;
+    }
+
     return (
         <div className="bkls-container">
-            <div className="bkls-book-list">
-                {booksInfo.map(b => (
-                    <Books key={b.book_id} booksInfo={b} />
+            <div className="bkls-books-list">
+                {booksInfo.map(m => (
+                    <Books key={m.id} booksInfo={m} />
                 ))}
             </div>
         </div>
     );
-}
+                }
+            }
 
 export default BookList;
---
+++ client/src/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Loading() {
+    return (
+        <div className="bkls-fullpage-text">
+            <p>Loading...</p>
+        </div>
+    );
+}
+
+export default Loading;
